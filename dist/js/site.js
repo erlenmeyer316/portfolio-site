820ac71c3de7dd4bf61aba97425d4c3953723a82
Adam Dickison <adam.dickison@gmail.com>
2019-02-11 23:32:49 -0500

    Further mobile adjustments

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     window.sr = ScrollReveal({
         reset: false,
-        duration: 600,
+        duration: 400,
         easing: 'cubic-bezier(.694,0,.335,1)',
         scale: 1,
-        viewFactor: 0.3
+        viewFactor: 0.1
     });
 
     sr.reveal('.background');
     sr.reveal('.skills');
     sr.reveal('.technical-summary');
-    sr.reveal('.experience');
     sr.reveal('.featured-projects');
-    sr.reveal('.other-projects');
+    sr.reveal('.experience');
+
 
 });
